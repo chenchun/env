b8069bd7d01afa7693eb7b0804d3b86372f95ad6
Chun Chen <chenchun.feed@gmail.com>
2014-08-13 13:17:25 +0800

    init

---
+++ bin/alias.bash
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
+    #alias dir='dir --color=auto'
+    #alias vdir='vdir --color=auto'
+
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+fi
+
+# some more ls aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+alias grep='grep --color=always'
+alias tree="find . -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'"
---
+++ bin/git-branch.bash
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+function parse_git_branch {
+    git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/' -e 's/((/(/' -e 's/))/)/'
+}
+function proml {
+    local GREEN="\[\033[0;32m\]"
+    local COLOR_END="\033[0m"
+    PS1="[${PS1}$GREEN\$(parse_git_branch)$COLOR_END]\$ "
+}
+proml
